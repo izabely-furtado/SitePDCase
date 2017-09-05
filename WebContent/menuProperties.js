b976c9e8745f812b9a5af0f733dd53dc0dd84732
Izabely Furtado <izabely_furtado@hotmail.com>
2017-09-05 16:23:41 -0300

    versao 35
    sessão de soluções novinha em folha
    :+1:  :100:

--- WebContent/menuProperties.js
+++ WebContent/menuProperties.js
@@ -188,12 +188,12 @@ function carregaMenuValores() {
 
 
 function carregaRodape() {
-	document.getElementById("rodape").innerHTML = 
-"		<footer>" +
-"			<div class='container-fluid footer-primary'>" +
+	document.getElementById("rodape").innerHTML =
+"		<footer style='background-color:black;'>" +
+"			<div class='container-fluid footer-primary' style='background-color:black;'>" +
 "			    <div class=''>" +
 "			      <div class='row' style='padding-top:10px; padding-bottom:-0px !important;'>" +
-"			        <div class='col-md-2' ><div class='logo-footer'><a href='./index.html'><img src='./images/logo-footer.png' class='img-responsive' /></a></div></div>" +
+"			        <div class='col-md-2' ><div class='logo-footer'><a href='./index.html'><center><img src='./images/logo-footer.png' class='img-responsive' /></center></a></div></div>" +
 "			        <div class='col-md-7' style=' text-align:center;'>" +
 "			          <p class='footer-address'><span class='text'>Alameda Oscar Niemeyer, nº 891 - Sala 708 - Vila da Serra - Belo Horizonte / Nova Lima - MG - CEP 34006-065</span><br /><a href='tel:(31) 3505-1940'>PDCASE ATENDE <span class='footer-tel'> (31) 3505-1940</span></a></p>" +
 "			        </div>" +
@@ -201,7 +201,7 @@ function carregaRodape() {
 "			      </div>" +
 "			    </div>" +
 "			 </div>" +
-"			 <div class='container-fluid footer-secondary' >" +
+"			 <div class='container-fluid footer-secondary' style='background-color:gold;'>" +
 "			 	<div class=''>" +
 "			 		<div class='row' style='padding-top:10px; padding-bottom:10px'>" +
 	"				    <div class='col-md-6' style='text-align:center; '>" +
